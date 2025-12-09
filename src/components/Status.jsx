2e5c31dd2c404b6511e30a49d5ca5cfe098d3203
Daniel <104313625+djtarulis@users.noreply.github.com>
2025-12-09 17:41:39 +0000

    chore: delete unnecessary files for previous socket testing

--- src/components/Status.jsx
+++
@@ -1,9 +0,0 @@
-import { useSocket } from '../contexts/SocketIOContext.jsx'
-export function Status() {
-  const { status, error } = useSocket()
-  return (
-    <div>
-      Socket status: <b>{status}</b> {error && <i> - {error.message}</i>}
-    </div>
-  )
-}
